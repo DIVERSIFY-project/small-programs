d76ee7db77694e6fc72a3bb8cdb314069c2a7617
Simon Allier <simon.allier@gmail.com>
2016-04-26 16:59:15 +0200

    add md5

---
+++ md5/src/test/java/md5/MD5Test.java
@@ -0,0 +1,34 @@
+package md5;
+
+import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
+import java.util.Random;
+
+/**
+ * User: Simon
+ * Date: 26/04/16
+ * Time: 13:58
+ */
+public class MD5Test {
+    protected int stringSizeMin = 50;
+    protected int stringSizeMax = 1000;
+    protected int nbTrial = 100;
+    protected Random random = new Random();
+
+    @Test
+    public void md5Test() {
+        for(int trial = 0; trial < nbTrial; trial++) {
+            String string = randomString();
+            assertArrayEquals(MD5.computeMD5(string.getBytes()), MD5Oracle.computeMD5(string.getBytes()));
+        }
+    }
+
+    protected String randomString() {
+        String string = "";
+        int stringSize = random.nextInt(stringSizeMax - stringSizeMin) + stringSizeMin;
+        for (int i = 0; i < stringSize; i++) {
+            string += ((char)random.nextInt(256));
+        }
+        return string;
+    }
+}
